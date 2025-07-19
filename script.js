dcf7051b7ea2801fb9eb664fac17746ef484772f
mrshaik65 <shaikmusthafa110223@gmail.com>
2025-07-19 14:10:29 +0530

    code updated

--- script.js
+++ script.js
@@ -1,70 +1,3 @@
-//  1. What is a variable in JavaScript?
-// Variable is a container which stores the data 
-
-// 2. How do you declare a variable using var? Give an example.
-// A var variable will declare with the "var" key word 
-// Ex: var a = 10
-// var name = "jhon"
-
-// 3. What is the difference between let and const?
-// With let variable we Declare, Assign, and reassign the values but the const variable not possible to reassigning because the const variable behaves like a constant nature.We can only declare and assign values using let variable.
-
-//  4. Can you reassign a value to a variable declared with const? Explain.
-// No we cannot reassign a value to a variable declared cost.When you declare a variable with const  it means that the variables reference cannot be changed. Once you assigned a value to const we cannot reassign a new value.
-
-//  5. What will happen if you use a variable before declaring it with let
-// It's thrown a error is called Reference error 
-
-// 6. What is variable hoisting? How does it work with var?
-// Variable hoisting is a JavaScript behavior where variables declared with var are 
-// moved to the top of their scope, regardless of where the actual declaration is made. 
-// This means that a variable can be used before it's declared, and it won't throw a ReferenceError.
-
-
-//  7. Write a code snippet that shows the block scope of let.
-// let x = 10; // global scope
-// if (true) {
-//   let x = 20; // block scope
-//   console.log(x); 
-// }
-// console.log(x); 
-
-// 8. Is it possible to declare multiple variables in a single line? Show how.
-
-// Yes
-// let x = 10, y = 20, z = 30;
-// console.log(x); 
-// console.log(y); 
-// console.log(z); 
-
-//  9. What is the default value of an uninitialized variable in JavaScript?
-// Undefined
-
-
-//  10. What is the difference between global and local variables? Give examples
-// Global variables are variables that are declared outside of any function or block scope.
-// They are accessible from anywhere in the code, including within functions and blocks.
-
-
-// let globalVariable = 10;
-// function myFunction() {
-//     console.log(globalVariable); // 10
-// }
-// myFunction();
-// console.log(globalVariable); // 10
-
-// ocal variables are variables that are declared within a function
-// or block scope. They are only accessible within that scope and are not accessible outside of it.
-
-// Example:
-// function myFunction() {
-//   let localVariable = 10;
-//   console.log(localVariable); // 10
-// }
-
-// myFunction();
-// console.log(localVariable); 
-
 1. 
 let age = 20
 console.log(typeof age)
@@ -136,11 +69,13 @@ console.log(sum)
 // let numMarks = Number(marks) + 15
 // console.log(numMarks)
 
+10.
 let message = "Hello, World!"
 message = 42
 console.log(message)
 console.log(typeof message)
 
+11.
 let text = "123abc"
 let numText = Number(text)
 console.log(numText)
